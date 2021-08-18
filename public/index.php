fc0b5b24d3d40b4268f69d4ef63e25b96149a64d
Andre Moura <andre.benedicto@etec.sp.gov.br>
2021-08-17 22:09:05 -0300

    inserir tipagem em Core

--- public/index.php
+++ public/index.php
@@ -4,6 +4,11 @@ use Source\Core\Core;
 
 session_start();
 
+// configurar headers para permitir o acesso de um domínio externo
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: jwt, Content-Type');
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $core = new Core();
