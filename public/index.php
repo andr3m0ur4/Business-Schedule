464d4275940cbab587dca3039f110a1d274e14a1
Andre Moura <andre.benedicto@etec.sp.gov.br>
2021-08-22 12:51:57 -0300

    isolar sessão dentro de uma classe

--- public/index.php
+++ public/index.php
@@ -1,8 +1,7 @@
 <?php
 
 use Source\Core\Core;
-
-session_start();
+use Source\Support\Session;
 
 // configurar headers para permitir o acesso de um domínio externo
 header('Access-Control-Allow-Origin: *');
@@ -10,6 +9,11 @@ header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Headers: jwt, Content-Type');
 
 require __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * BOOTSTRAP
+ */
+$session = new Session();
 
 $core = new Core();
 $core->run();
