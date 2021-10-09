ac7c842addc1a5a8ed026d0828739802b273dd82
Andre Moura <andre.benedicto@etec.sp.gov.br>
2021-10-09 01:53:25 -0300

    corrigir apresentação da data

--- source/Models/TvShow.php
+++ source/Models/TvShow.php
@@ -45,13 +45,11 @@ class TvShow extends Model
 
     public function getDate() : ?string
     {
-        // return date_format_br($this->date);
         return $this->date;
     }
 
     public function setDate($date)
     {
-        // $this->date = date_format_app($date);
         $this->date = $date;
         return $this;
     }
