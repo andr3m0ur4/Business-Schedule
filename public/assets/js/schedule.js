beef707f4394e75bebe09c54673169f1eea07b62
Andre Moura <andre.benedicto@etec.sp.gov.br>
2021-11-13 15:39:01 -0300

    melhorar botão para fechar alert

--- public/assets/js/schedule.js
+++ public/assets/js/schedule.js
@@ -72,15 +72,7 @@ const initCards = card => {
 
         if (dbEmployeeTime.has(idEmployeeTime)) {
             const employeeTime = dbEmployeeTime.get(idEmployeeTime)
-
-            const startTime = document.createElement('span')
-            startTime.innerHTML = employeeTime.startTime
-            const finalTime = document.createElement('span')
-            finalTime.innerHTML = employeeTime.finalTime
-
-            card.querySelector('.card-header').innerHTML = ''
-            card.querySelector('.card-header').appendChild(startTime)
-            card.querySelector('.card-header').appendChild(finalTime)
+            fillCardEmployeeTime(card, employeeTime)
         }
 
         card.onclick = e => {
@@ -226,6 +218,17 @@ const fillModalEmployeeTime = employee => {
     document.getElementById('saveTime').onclick = saveTime
 }
 
+const fillCardEmployeeTime = (card, employeeTime) => {
+    const startTime = document.createElement('span')
+    startTime.innerHTML = employeeTime.startTime
+    const finalTime = document.createElement('span')
+    finalTime.innerHTML = employeeTime.finalTime
+    
+    card.querySelector('.card-header').innerHTML = ''
+    card.querySelector('.card-header').appendChild(startTime)
+    card.querySelector('.card-header').appendChild(finalTime)
+}
+
 const saveTime = e => {
     const btn = e.target;
     const form = document.querySelector('[name=formEmployeeTime]')
@@ -258,16 +261,9 @@ const validateInputs = e => {
 
 const updateCard = id => {
     const employeeTime = dbEmployeeTime.get(id)
-    
-    const startTime = document.createElement('span')
-    startTime.innerHTML = employeeTime.startTime
-    const finalTime = document.createElement('span')
-    finalTime.innerHTML = employeeTime.finalTime
-    
+
     const card = document.querySelector(`.card[data-card-time="${id}"]`)
-    card.querySelector('.card-header').innerHTML = ''
-    card.querySelector('.card-header').appendChild(startTime)
-    card.querySelector('.card-header').appendChild(finalTime)
+    fillCardEmployeeTime(card, employeeTime)
 }
 
 const submitTvShowHour = e => {
