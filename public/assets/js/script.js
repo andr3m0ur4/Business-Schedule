a320cac17c0f0b48155f5e54867676c72165cbe5
Andre Moura <andre.benedicto@etec.sp.gov.br>
2021-11-22 15:50:47 -0300

    função para formatar Hora

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,3 +3,10 @@ const clearPostJS = () => {
         window.history.replaceState(null, null, window.location.href)
     }
 }
+
+const formatTime = time => {
+    return new Intl.DateTimeFormat('pt-BR', {
+        hour: 'numeric',
+        minute: 'numeric'
+    }).format(new Date(time))
+}
