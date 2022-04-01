8c013487b31cf3cf8507e30f79410bc66543ef31
André Moura <andre.benedicto@etec.sp.gov.br>
2022-03-31 23:13:45 -0300

    atualizar tamanho da coluana name da tabela studios

--- database/migrations/2022_04_01_001014_create_studios_table.php
+++ database/migrations/2022_04_01_001014_create_studios_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('studios', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 100);
             $table->timestamps();
             $table->softDeletes();
         });
