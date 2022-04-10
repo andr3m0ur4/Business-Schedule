5e29f2b067c7861ff2d0abee20de3e24d5534853
André Moura <andre.benedicto@etec.sp.gov.br>
2022-04-09 21:27:12 -0300

    adicionar componente principal adicionar e isolar componente de header

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import '@/assets/css/backend-plugin.min.css'
+import '@/assets/css/backend.css'
+import '@/assets/vendor/@fontawesome/fontawesome-free/css/all.min.css'
+import '@/assets/vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css'
+import '@/assets/vendor/remixicon/fonts/remixicon.css'
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
