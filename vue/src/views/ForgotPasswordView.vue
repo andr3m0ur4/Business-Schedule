ced1e0d289d4df343dcd8109e17c0a6b0fdef055
Rodrigo Ramos de Freitas <rodrigoramos2002@outlook.com>
2022-04-23 12:03:23 -0300

    Adicionando envio de emails

--- vue/src/views/ForgotPasswordView.vue
+++ vue/src/views/ForgotPasswordView.vue
@@ -12,13 +12,14 @@
                            <div class="row">
                               <div class="col-lg-12">
                                  <div class="floating-input form-group">
-                                    <input class="form-control" type="text" name="email" id="email" required />
-                                    <label class="form-label" for="email">Email</label>
+                                    <input class="form-control" type="mail" name="mail" id="mail" v-model="mail" placeholder="Email" required />
+                                    <label class="form-label" for="mail">Email</label>
                                  </div>
                               </div>
                            </div>
-                           <button type="submit" class="btn btn-primary">Redefinir</button>
+                           <button type="button" @click="sendMail()" class="btn btn-primary">Redefinir</button>
                         </form>
+                        {{mail}}
                      </div>
                   </div>
                </div>
@@ -27,3 +28,31 @@
       </section>
       </div>
 </template>
+
+<script>
+import axios from '@/axios'
+
+export default {
+  name: 'ForgotPasswordView',
+  data() {
+    return {
+      mail:null
+    }
+  },
+  methods: {
+   sendMail() {
+      console.log(this.mail)
+         axios.get('email/sendMail', {
+         params: {mail: this.mail}
+      })
+         .then(response => {
+            this.$swal('Sucesso', `E-mail enviado para ${response.data.email}`, 'success')
+         })
+         .catch(error => {
+
+           console.log(error.response)
+         })
+      }
+   }
+}
+</script>
