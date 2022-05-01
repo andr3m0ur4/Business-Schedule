12ed25d6fbb96f22cee6d9be98f7796421faa662
Igorsan539 <igorsan539@gmail.com>
2022-05-01 10:23:45 -0300

    View confirm password

--- vue/src/router/routes.js
+++ vue/src/router/routes.js
@@ -14,6 +14,7 @@ import ForgotPasswordView from '@/views/ForgotPasswordView'
 import BlankPage from '@/views/BlankPage'
 import UICalendar from '@/views/UICalendar'
 import ResetPasswordEmail from '@/views/ResetPasswordEmail'
+import ConfirmPasswordView from '@/views/ConfirmPasswordView'
 import store from '@/store'
 
 const routes = [
@@ -89,6 +90,11 @@ const routes = [
         path: '/email',
         name: 'email',
         component: ResetPasswordEmail
+    },
+    {
+        path: '/confirma-senha',
+        name: 'confirma-senha',
+        component: ConfirmPasswordView
     }
 ]
 
