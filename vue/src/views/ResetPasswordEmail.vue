89ccb9aa968b3614e82d4a994509996caec31025
Igorsan539 <igorsan539@gmail.com>
2022-05-08 16:28:22 -0300

    Ajuste de layout

--- vue/src/views/ResetPasswordEmail.vue
+++ vue/src/views/ResetPasswordEmail.vue
@@ -1,24 +1,24 @@
 <template>
-  <div class="">
-    <div class="wrapper">
-      <section class="login-content">
-         <div class="container h-100">
-            <div class="row justify-content-center align-items-center height-self-center">
-               <div class="col-md-5 col-sm-12 col-12 align-self-center">
-                  <div class="card">
-                     <div class="card-body text-center">
-                         <img src="../assets/images/bs-icon-high.png" height="130" width="130">
-                         <div class="card-title">
-                            <h2>Redefinir senha</h2>
-                         </div>
+   <div class="">
+      <div class="wrapper">
+         <section class="login-content">
+            <div class="container h-100">
+               <div class="row justify-content-center align-items-center height-self-center">
+                  <div class="col-md-5 col-sm-12 col-12 align-self-center">
+                     <div class="card">
+                        <div class="card-body text-center">
+                           <img src="../assets/images/bs-icon-high.png" height="130" width="130">
+                           <div class="card-title">
+                              <h2>Redefinir senha</h2>
+                           </div>
                            <p>Ao clicar no botão abaixo você será redirecionado para a tela de redefinição de senha.</p>
                            <button type="submit" class="btn btn-primary">Redefinir</button>
+                        </div>
                      </div>
                   </div>
                </div>
             </div>
-         </div>
-      </section>
-    </div>
-  </div>
+         </section>
+      </div>
+   </div>
 </template>
