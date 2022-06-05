61fa514e75bf119fd9281ad36f11d8dbeaa5e904
Igorsan539 <igorsan539@gmail.com>
2022-06-05 19:22:22 -0300

    ajuste na modal

--- vue/src/components/ChatModal.vue
+++ vue/src/components/ChatModal.vue
@@ -10,39 +10,37 @@
             <div>
               <div class="media align-items-top user-detail mb-1">
                 <div class="row">
-                  <div class="col-12"><h6>{{employee.name}}</h6></div>
-                  <div class="col-12"><div class="badge badge-color ml-3 mt-0">Owner</div></div>
+                  <div class="col-12">
+                    <h6>{{ employee.name }}</h6>
+                  </div>
+                  <div class="col-12">
+                    <div class="badge badge-color ml-3 mt-0">Owner</div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
-          <div class="col-md-12">
-            <fieldset>
-              <div class="form-card text-left">
-                <div class="row">
-                  <div class="col-md-12">
-                    <div class="form-group">
-                      <textarea class="form-control" id="exampleFormControlTextarea3" rows="7">
-                      </textarea>
-                    </div>
-                  </div>
-                </div>
+          <div class="col-12 border rounded">
+            <div class="scrollspy-example" data-spy="scroll" data-target="#navbar-example2" data-offset="0">
+              <h4 id="fat">Igor</h4>
+              <div v-for="message_local in messages" :key="message_local.id">
+                <p>{{ message_local.message }}</p>
               </div>
-            </fieldset>
+              <h4 id="mdo">Rodrigo</h4>
+              <p>Oi</p>
+            </div>
           </div>
-            <div v-for="message_local in messages" :key="message_local.id">
-            <p>{{message_local.message}}</p>
-          </div>
-          <div class="col-md-12">
+          <div class="mt-2">
             <form id="form-wizard" class="text-center" @submit.prevent="event(id)">
               <div class="row">
                 <div class="form-group col-md-9">
-                  <input type="text" class="form-control" id="message" name="message" :value="message" @input="updateValue" placeholder="Enviar mensagem" required="required" />
+                  <input type="text" class="form-control" id="message" name="message" :value="message"
+                    @input="updateValue" placeholder="Enviar mensagem" required="required" />
                 </div>
                 <div class="col-md-3">
                   <button type="button" class="btn btn-primary" @click="event(id)">Enviar</button>
@@ -84,12 +82,21 @@ export default {
     }
   },
   mounted() {
-    $('#addChat, #updateChat').on('shown.bs.modal', function(e) {
+    $('#addChat, #updateChat').on('shown.bs.modal', function (e) {
       $(e.target).find('[message]').focus();
     });
-    $('#addChat, #updateChat').on('hide.bs.modal', function() {
+    $('#addChat, #updateChat').on('hide.bs.modal', function () {
       $('#form-wizard').trigger('reset');
     })
   }
 }
 </script>
+
+<style scoped>
+.scrollspy-example {
+  position: relative;
+  height: 200px;
+  margin-top: 0.5rem;
+  overflow: auto;
+}
+</style>
